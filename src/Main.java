d494e3265706fe49b25f8c965f9fc2017f440f83
toyondiamond <eplmpopova@mail.ru>
2022-12-22 10:20:50 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,61 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printIsleapYear(2023);
+        recommendOS(1, 2020);
+        calculateDeliveryDays(50);
+    }
+
+
+    //task1
+    public static void printIsleapYear(int year) {
+        boolean yearIsLeap = isLeap(year);
+        printIsLeapYearResult(year, yearIsLeap);
+    }
+
+    public static boolean isLeap(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    public static void printIsLeapYearResult(int year, boolean yearIsLeap) {
+        if (yearIsLeap) {
+            System.out.println(year + " високосный год");
+        } else {
+            System.out.println(year + " не является вискосным годом");
+        }
+    }
+
+    //task2
+    public static void recommendOS(int clientOS, int deviceYear) {
+        boolean deviceIsOld = isDeviceOld(deviceYear);
+        System.out.print("Установите ");
+        if (deviceIsOld) {
+            System.out.print(" Lite");
+        }
+        System.out.print(" версию для ");
+        if (clientOS ==0) {
+            System.out.println("iOS");
+        }else {
+            System.out.println("Android");
+        }
+    }
+    private static boolean isDeviceOld(int deviceYear ){
+        int currentYear = LocalDate.now().getYear();
+        return deviceYear<= currentYear;
+    }
+
+    //task3
+    public static void calculateDeliveryDays (int deliveryDistance){
+        int result = 1;
+        if (deliveryDistance > 20) {
+            result++;
+        }
+        if (deliveryDistance <60 && deliveryDistance <=100) {
+            result++;
+        }else {
+            System.out.println("Невозможно рассчитать срок доставки");
+        }
+        System.out.println("Необходимо дней " + result);
     }
 }
